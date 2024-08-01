03e390e821bf9fc72d64627ef5daa21ec1bc1da8
kristoff <706060951@qq.com>
2024-08-01 09:57:34 +0800

    240801

--- tools/merge_no_overlap_reads/src/main.rs
+++
@@ -1,9 +0,0 @@
-use bio::io::fastq;
-
-let fq: &'static [u8] = b"@id description\nACGT\n+\n!!!!\n";
-let records = fastq::Reader::new(fq)
-    .records()
-    .map(|record| record.unwrap());
-for record in records {
-    assert!(record.check().is_ok())
-}
