14b9e7b587f47c101380fda86a5a35414e2236a8
kristoff <706060951@qq.com>
2024-09-06 14:28:18 +0800

    240906

--- jml_tcr/config.py
+++ jml_tcr/config.py
@@ -6,7 +6,7 @@ import logging
 def get_software_dict() -> dict:
     """获取软件字典"""
     logging.info('获取软件字典')
-    yaml_soft = Path(__file__).resolve().parent.joinpath('config/software.yaml')
+    yaml_soft = Path(__file__).resolve().parents[1].joinpath('config/software.yaml')
 
     with open(yaml_soft) as f:
         dict_soft = yaml.safe_load(f)
@@ -17,7 +17,7 @@ def get_software_dict() -> dict:
 def get_database_dict() -> dict:
     """获取数据库字典"""
     logging.info('获取数据库字典')
-    yaml_db = Path(__file__).resolve().parent.joinpath('config/database.yaml')
+    yaml_db = Path(__file__).resolve().parents[1].joinpath('config/database.yaml')
 
     with open(yaml_db) as f:
         dict_db = yaml.safe_load(f)
@@ -28,7 +28,7 @@ def get_database_dict() -> dict:
 def get_environment_dict() -> dict:
     """获取 Conda 环境字典"""
     logging.info('获取 Conda 环境字典')
-    yaml_env = Path(__file__).resolve().parent.joinpath('config/environment.yaml')
+    yaml_env = Path(__file__).resolve().parents[1].joinpath('config/environment.yaml')
     with open(yaml_env) as f:
         dict_env = yaml.safe_load(f)
 
